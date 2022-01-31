6382ec9744924b05a19ccebdfd1d84209cdea1a3
Michael Shu <michael.shu@thermovisionusa.com>
2022-01-31 13:56:18 +0800

    Upgrade @prisma/client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
 // Learn more: https://pris.ly/d/help/next-js-best-practices
-// Nrxt.js fast refresh. It create an instance everytime hitting save.
+// Next.js fast refresh. It create an instance everytime hitting save.
 
 let prisma: PrismaClient
 
